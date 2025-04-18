cc3f2a75d452c3f97299f9bf2c0a47776bb853f3
PhoenixFury0000 <piyushguptaofficial07042009@gmail.com>
2025-04-19 05:06:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,25 +2,25 @@ const fs = require('fs')
 const chalk = require('chalk')
 //Feel free to customize my bot informations to yours. Starting from this point
 //owmner v card
-global.ytname = "YT: ElitePro" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.ytname = "PhøenixFury" //ur yt chanel name
+global.socialm = "PhoenixFury0000" //ur github or insta name
+global.location = "india" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
-global.websitex = "https://youtube.com/@elitepro-empire"
-global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
+global.botname = 'yurei bot' //ur bot name
+global.ownernumber = '917973456275' //ur owner number
+global.ownername = 'PhøenixFury' //ur owner name
+global.websitex = "https://youtube.com/@phoenixfury_editz0001"
+global.elitepropp = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLndAvutW3hJc2YPuHTUTIy3D9fzkNhS20meCvT1lxnPSRRvsifVdjbLI&s=10' // Change this image link to yours.
 
-global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
-global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
+global.themeemoji = '😎'
+global.wm = "yurei bot"
+global.botscript = ' hmmm' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "yurei bot\n\n+917973456275"
+global.creator = "917973456275@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["917973456275"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
